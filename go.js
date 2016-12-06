5c68193a35c89f7ada82e4d3b0ef44706b6b17a1
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2016-12-06 14:48:26 +0100

    challenged completed

--- go.js
+++ go.js
@@ -6,6 +6,17 @@ const app = express()
 
 
 // routes
+app.get('/:thestring', (req, res) => {
+  let code = 200, timestamp = null, natural = null
+
+  const date = new Date(req.params.thestring)
+  if (!isNaN(date.getTime())) {
+    timestamp = date.getTime()
+    natural = date.toDateString()
+  } else code = 400
+
+  res.status(code).json({ timestamp, natural })
+})
 
 
 // go!
